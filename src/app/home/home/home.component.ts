238e4edfa3099407e71230584d83f0c3e2148e97
张乐平 <holynull@126.com>
2020-05-25 14:34:55 +0800

    tabs

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -6,7 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
+    slideOpts = {
+        // init: false,
+        speed: 400,
+        initialSlide: 0,
+        // loop: true,
+        // observer: true, // 修改swiper自己或子元素时，自动初始化swiper
+        // observeParents: true, // 修改swiper的父元素时，自动初始化swiper
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false
+        }, // 可选选项，自动滑动
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    };
   constructor() { }
 
   ngOnInit() {}
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonTabs } from '@ionic/angular';
 
 @Component({
-  selector: 'app-tabs',
-  templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+    selector: 'app-tabs',
+    templateUrl: 'tabs.page.html',
+    styleUrls: ['tabs.page.scss']
 })
 export class TabsPage {
 
-  constructor() {}
+    @ViewChild('tabs', null)
+    tabs: IonTabs
+
+    curTab: string
+
+    constructor() { }
+
+    tabChanged($event) {
+        this.curTab = $event.tab;
+    }
 
 }
