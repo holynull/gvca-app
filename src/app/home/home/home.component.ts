543450b63d3d77cdccada4c7a03b8d8206ec3cc3
张乐平 <holynull@126.com>
2020-05-26 15:11:38 +0800

    解决了slides停止自动播放的问题

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'app-home',
@@ -15,7 +17,9 @@ export class HomeComponent implements OnInit {
         // observeParents: true, // 修改swiper的父元素时，自动初始化swiper
         autoplay: {
             delay: 3000,
-            disableOnInteraction: false
+            disableOnInteraction: false,
+            waitForTransition: false,
+            reverseDirection: true,
         }, // 可选选项，自动滑动
         pagination: {
             el: '.swiper-pagination',
@@ -24,14 +28,34 @@ export class HomeComponent implements OnInit {
     };
     noticeOpts = {
         speed: 400,
-        loop: true,
+        // loop: true,
         autoplay: {
             delay: 5000, // 1秒切换一次
-            disableOnInteraction: false
+            disableOnInteraction: false,
+            waitForTransition: false,
+            reverseDirection: true,
         }, // 可选选项，自动滑动
         direction: 'vertical',
     };
-    constructor() { }
+
+    @ViewChild('bannerSlides', null)
+    bannerSlides: IonSlides;
+
+    @ViewChild('noticeSlides', null)
+    noticeSlides: IonSlides;
+
+    constructor(
+        private activeRoute: ActivatedRoute
+    ) {
+        this.activeRoute.queryParams.subscribe(param => {
+            // if (this.bannerSlides) {
+            //     this.bannerSlides.slideNext().then();
+            // }
+            // if (this.noticeSlides) {
+            //     this.noticeSlides.slideNext().then();
+            // }
+        });
+    }
 
     ngOnInit() { }
 
