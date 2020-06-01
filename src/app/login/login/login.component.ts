6826a3280ce55a7860fd6b51d9023ad72b3782c8
张乐平 <holynull@126.com>
2020-06-01 17:43:43 +0800

    登录页面

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,14 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {}
+    userName: string;
+
+    pwd: string;
+
+    constructor(private alertCtrl: AlertController) { }
+
+    ngOnInit() { }
+
+    login() {
+        if (!this.userName || !this.pwd) {
+
+        } else {
+
+        }
+    }
 
 }
