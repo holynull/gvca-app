ee8f9ead0997019faac652cfe06514254bd09dc8
张乐平 <holynull@126.com>
2020-07-18 20:04:34 +0800

    bug

--- src/app/services/course.service.ts
+++ src/app/services/course.service.ts
@@ -49,7 +49,9 @@ export class CourseService {
     }
 
     loadCourse(type: number, pageInfo: PageInfo, callbak?: (state: CourseSelectStates) => any) {
-        this.courses.splice(0,this.courses.length);
+        if (pageInfo.curPageNum === 1) {
+            this.courses.splice(0, this.courses.length);
+        }
         this.api.getCourseList(type, pageInfo.curPageNum, pageInfo.pageSize).subscribe(res => {
             if (res.code === 1) {
                 if (pageInfo.curPageNum === 1) {
