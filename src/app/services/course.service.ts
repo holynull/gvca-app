eb93d35ded11700e99870660ebde0ebd2d0ff495
张乐平 <holynull@126.com>
2020-07-18 12:50:58 +0800

    课程默认图片

--- src/app/services/course.service.ts
+++ src/app/services/course.service.ts
@@ -49,6 +49,7 @@ export class CourseService {
     }
 
     loadCourse(type: number, pageInfo: PageInfo, callbak?: (state: CourseSelectStates) => any) {
+        this.courses.splice(0,this.courses.length);
         this.api.getCourseList(type, pageInfo.curPageNum, pageInfo.pageSize).subscribe(res => {
             if (res.code === 1) {
                 if (pageInfo.curPageNum === 1) {
